809b62b1d81aabdc0d59f22314a1c1a2908dc2cb
Alexander Fritze <alexander.fritze@gmx.de>
2025-01-05 13:14:00 +0100

    pylsp

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt-get --yes install software-properties-common
 sudo apt-get --yes install fzf
 sudo apt-get --yes install ninja-build
 sudo apt-get --yes install build-essential
+sudo apt-get --yes install 'python3-pylsp*'
 sudo apt --yes install gcc-10 gcc-10-base gcc-10-doc g++-10
 sudo apt --yes install libstdc++-10-dev libstdc++-10-doc
 
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,19 +1,31 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-	library = {
-	  -- See the configuration section for more details
-	  -- Load luvit types when the `vim.uv` word is found
-	  { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	},
-      },
-    },
-    config = function()
-      require("lspconfig").lua_ls.setup {}
-    end,
-  }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
+			require("lspconfig").pylsp.setup{
+			--	settings = {
+			--		pylsp = {
+			--			plugins = {
+			--				pycodestyle = {
+			--					ignore = {'W391'},
+			--					maxLineLength = 100
+			--				}
+			--			}
+			--		}
+			--  }
+			}
+		end,
+	}
 }
