c9e0cedb8b1882c70706afbbc175fce0acd57aee
Alexander Fritze <alexander.fritze@gmx.de>
2025-01-05 12:26:01 +0100

    Basic install script for nvim and dependencies

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get --yes update
+sudo apt-get --yes upgrade
+sudo apt-get --yes install git
+sudo apt-get --yes install software-properties-common
+sudo apt-get --yes install fzf
+sudo apt-get --yes install ninja-build
+sudo apt-get --yes install build-essential
+sudo apt --yes install gcc-10 gcc-10-base gcc-10-doc g++-10
+sudo apt --yes install libstdc++-10-dev libstdc++-10-doc
+
+sudo snap install --beta nvim --classic
