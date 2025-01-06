9a7f8b5b053862a980289749da497e4821471ee1
Alexander Fritze <alexander.fritze@gmx.de>
2025-01-06 16:56:32 +0100

    install.sh - lua language server, snapd

--- install.sh
+++ install.sh
@@ -9,6 +9,15 @@ sudo apt-get --yes install 'python3-pylsp*'
 sudo apt --yes install gcc-10 gcc-10-base gcc-10-doc g++-10
 sudo apt --yes install libstdc++-10-dev libstdc++-10-doc
 
+mkdir -p ~/thirdparty
+pushd  ~/thirdparty
+git clone https://github.com/LuaLS/lua-language-server
+cd lua-language-server
+./make.sh
+sudo ln -s ~/thirdparty/lua-language-server/bin/lua-language-server /usr/local/bin/lua-language-server
+popd
+
+sudo apt --yes install snapd
 sudo snap install bash-language-server --classic
 sudo snap install go --classic
 go install golang.org/x/tools/gopls@latest
