1b9ce698d4d05b4bb67be93e120fdf64f65cda0d
Fritze <alexander.fritze@zuehlke.com>
2025-01-07 13:46:12 +0100

    Some adaptations to support win and only python lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,33 +1,31 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
-		dependencies = {
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
+		-- dependencies = {
+		--	"folke/lazydev.nvim",
+		-- ft = "lua", -- only load on lua files
+		--	opts = {
+		--		library = {
+		--			-- See the configuration section for more details
+		--			-- Load luvit types when the `vim.uv` word is found
+		--			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+		--		},
+		--	},
+		-- },
 		config = function()
-			require("lspconfig").lua_ls.setup {}
 			require("lspconfig").pylsp.setup{
-			--	settings = {
-			--		pylsp = {
-			--			plugins = {
-			--				pycodestyle = {
-			--					ignore = {'W391'},
-			--					maxLineLength = 100
-			--				}
-			--			}
-			--		}
-			--  }
+				settings = {
+					pylsp = {
+						virtual_text = true,
+						plugins = {
+							pycodestyle = {
+								ignore = {'W391'},
+								maxLineLength = 100
+							}
+						}
+			  	}
+				}
 			}
-			require("lspconfig").bashls.setup{}
-			require("lspconfig").gopls.setup{}
 		end,
 	}
 }
