8193464603cce7b5df543885dfa2e490af47fa84
Alexander Fritze <alexander.fritze@gmx.de>
2025-01-18 18:20:41 +0100

    formatting

--- lua/config/plugins/zig.lua
+++ lua/config/plugins/zig.lua
@@ -1,14 +1,15 @@
 return {
-  {
-      'ziglang/zig.vim', version = '*',
-      config = function()
-				vim.api.nvim_create_autocmd('BufWritePre', {
-					group = vim.api.nvim_create_augroup('Zig', { clear = true }),
-					pattern = { "*.zig", "*.zon" },
-					callback = function()
-						vim.lsp.buf.format()
-					end,
-				})
-      end
-  }
+	{
+		'ziglang/zig.vim',
+		version = '*',
+		config = function()
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('Zig', { clear = true }),
+				pattern = { "*.zig", "*.zon" },
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	}
 }
